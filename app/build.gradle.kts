b94a11c4a9ef0c01a6422769a510bbeac51642d8
Dan Grover <daniel.grover@csuglobal.edu>
2025-03-16 15:35:42 -0700

    fix build settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dangrover.danphotogallery"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dangrover.danphotogallery"
-        minSdk = 31
-        targetSdk = 34
+        minSdk = 32
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
