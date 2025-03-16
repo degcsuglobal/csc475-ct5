326a03a0375d7a62d39741fe655ac0f30b3ce9ee
Dan Grover <daniel.grover@csuglobal.edu>
2025-03-16 16:51:26 -0700

    initial progress on getting grid view to work

--- app/src/main/java/com/dangrover/danphotogallery/FirstFragment.kt
+++ app/src/main/java/com/dangrover/danphotogallery/FirstFragment.kt
@@ -1,11 +1,18 @@
 package com.dangrover.danphotogallery
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import android.view.*
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.dangrover.danphotogallery.databinding.FragmentFirstBinding
 
 /**
@@ -36,7 +43,61 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }*/
 
+        binding.recycler.layoutManager = GridLayoutManager(requireContext(), 3)
+
+        val adapter = PhotoAdapter(requireContext())
+
+        binding.recycler.adapter = adapter
+
     }
+
+
+    // the photos are all stored in the assets folder, there are 10
+
+    class PhotoAdapter(
+        private val context: Context,
+   //     private val onItemClickListener: OnItemClickListener
+    ) : RecyclerView.Adapter<PhotoViewHolder>() {
+
+        /*
+        interface OnItemClickListener {
+            fun onItemClick(photoId: Int)
+        }*/
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
+            return PhotoViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+            // generate photo ID and drawable from path, they are all numbered numerically as e.g. "photo0.png" in drawable and stored as pngs.
+
+            val photoId = position + 1
+            val drawableName = "photo$photoId"
+            val drawableResId = context.resources.getIdentifier(drawableName, "drawable", context.packageName)
+
+            Log.d("PhotoAdapter", "Photo ID: $photoId")
+
+
+
+            // make the drawable
+            val drawable: Drawable? = ContextCompat.getDrawable(context, drawableResId)
+            holder.photoImageView.setImageDrawable(drawable)
+
+         /*   holder.cardView.setOnClickListener {
+                //onItemClickListener.onItemClick(photoId)
+            }*/
+        }
+
+        override fun getItemCount(): Int = 6
+    }
+
+
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val photoImageView: ImageView = itemView.findViewById(R.id.photoImageView)
+        val cardView: CardView = itemView.findViewById(R.id.cardView)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
